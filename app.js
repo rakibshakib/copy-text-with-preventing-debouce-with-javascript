2387606a44247d55cc481c699219070e8335a3f8
rakibshakib <rakibshakib74@gmail.com>
2022-02-06 17:11:04 +0600

    added text copy function

---
+++ app.js
@@ -0,0 +1,32 @@
+const button = document.getElementById('clicked-btn');
+const demoText = document.getElementById('demoText');
+
+
+function debounce(func, delay) {
+    let pendingTimeOut;
+    return function () {
+        if (pendingTimeOut) {
+            clearInterval(pendingTimeOut);
+        }
+        pendingTimeOut = setTimeout(() => {
+            func();
+        }, delay);
+    };
+} 
+
+function copyToClipBoard(text){
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+}
+
+button.addEventListener(
+    'click',
+    debounce(function () {
+        console.log('Text Coppied!');
+        copyToClipBoard(demoText.innerText);
+    }, 1000)
+);
